63f29f5efb1f5d68a5f7ea6a3fc717dbe8e21074
umer936 <umer936@gmail.com>
2016-08-17 18:30:37 -0500

    Yay an icon

--- submit.js
+++ submit.js
@@ -1,6 +1,9 @@
 document.getElementById("Submit").addEventListener("click", submitTab);
 
 function submitTab() {
-    var newURL = "http://PassReqs.umer936.com/submit.php";
+    chrome.tabs.query({active:true,currentWindow:true},function(tab){
+        var url = tab[0].url;
+        var newURL = "http://PassReqs.umer936.com/submit.php?site=" + url;
     chrome.tabs.create({ url: newURL });
+	});
 };
